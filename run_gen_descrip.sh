94c757246be3d002c6157eff02b04d415f5c5489
yukun feng <yukunfg@gmail.com>
2020-08-17 21:42:20 +0900

    emb descrip

---
+++ run_gen_descrip.sh
@@ -0,0 +1,6 @@
+python ./descrip_emb_util.py \
+    --data_dir "data/fewrel" \
+    --ernie_model "bert_base" \
+    --entities_tsv "/home/lr/yukun/kg-bert/entities.slimed.tsv" \
+    --do_lower_case \
+    --threshold 0.0
