79eaca6ba597a510a72d854ce5d952b506e6e17b
yukun feng <yukunfg@gmail.com>
2020-08-19 11:15:55 +0900

    add sh

---
+++ run_fewrel_with_descrip.sh
@@ -0,0 +1,29 @@
+set -x
+
+# model="ernie_base"
+# model="bert_base_under_ernie"
+# model="bert_wwm_base"
+# model="bert-large-wwm"
+# model="bert_standard"
+# model="bert_descrip_same_time"
+# model="bert_descrip_same_time_under_ernie"
+# model="bert_descrip_same_time_contra"
+# model="bert_descrip_as_input"
+# model="roberta-base"
+model="bert_base"
+
+data="data/fewrel"
+
+emb_base="fewrel_descrip_emb"
+max_parent=3
+
+# output="${model}_$(basename $data)_output"
+output="${model}_$(basename $data)_output_debug"
+rm -rf $output
+
+
+python3 code/run_fewrel_with_descrip.py --max_parent $max_parent --emb_base $emb_base  --do_train   --do_lower_case   --data_dir $data   --ernie_model $model   --max_seq_length 256   --train_batch_size 16   --learning_rate 2e-5   --num_train_epochs 10   --output_dir $output      --loss_scale 128 --entities_tsv "/home/lr/yukun/kg-bert/entities.slimed.tsv"
+# evaluate
+# python3 code/eval_fewrel.py   --do_eval   --do_lower_case   --data_dir $data   --ernie_model $model   --max_seq_length 256   --train_batch_size 32   --learning_rate 2e-5   --num_train_epochs 10   --output_dir $output      --loss_scale 128
+
+# python ~/env_config/sending_emails.py -c "$0 succ: $? $model finished;"
